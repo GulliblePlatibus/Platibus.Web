096dee6c5946fd88ef833530ef323bc4e4ebfc4c
Oskar Skak Kristiansen <skakkristiansen@gmail.com>
2018-10-23 16:43:53 +0200

    Created a place to register users, and changes certain layout elements

---
+++ Platibus.Web/Pages/Register.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Platibus.Web.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        /*Below is the OnPost method for the different elements
+         * In these methods, the two-way binding is used
+         * To access the bound object, simply use the name specified in the .cshtml file
+         */ 
+        public void OnPost(string user_emailaddress)
+        {
+            
+        }
+    }
+}
