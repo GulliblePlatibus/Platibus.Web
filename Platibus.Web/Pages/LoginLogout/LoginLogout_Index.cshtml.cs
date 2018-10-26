b13804c8f8e3360dc89c9b64e0a3ea4817ad2320
Ulrik Sandberg <ulrik.sandberg@champagnemoments.eu>
2018-10-26 17:44:39 +0200

    Clearing cookies really effectively

--- Platibus.Web/Pages/LoginLogout/LoginLogout_Index.cshtml.cs
+++ Platibus.Web/Pages/LoginLogout/LoginLogout_Index.cshtml.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Platibus.Web.Pages.LoginLogout
 {
+    [Authorize]
     public class LoginLogout_IndexModel : PageModel
     {
+        private const string Cookie = ".AspNetCore.Cookies";
+        
         public void OnGet()
         {
+        
+        }
 
+        public async void OnPost()
+        {
+            foreach (var cookie in Request.Cookies.Keys)
+            {
+                Response.Cookies.Delete(cookie);
+            }
+            
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
         }
     }
 }
