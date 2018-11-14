ff9ee521fbca73e64ce69f64fde4f989cd2a81d6
Oskar Skak Kristiansen <skakkristiansen@gmail.com>
2018-11-14 15:42:42 +0100

    Userinformation and update user stuff (Needs change with identity user fully implemented!!)

---
+++ Platibus.Web/Pages/Personal/EditInformation.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Platibus.Web.Acquaintance.IDataServices;
+using Platibus.Web.DataServices.Models.User;
+
+namespace Platibus.Web.Pages.Personal
+{
+    public class EditInformationModel : PageModel
+    {
+        private IUserDataService _userDataService;
+        [BindProperty] public User _user { get; set; }
+
+        public EditInformationModel(IUserDataService userDataService)
+        {
+            _userDataService = userDataService;
+        }
+
+        
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            _user = await _userDataService.GetUserById(Guid.Parse(id));
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostUpdateUserAsync(string id)
+        {
+            var response = await _userDataService.UpdateUserById(Guid.Parse(id), _user);
+
+            if (!response.IsSuccesfull)
+            {
+                return new RedirectToPageResult("Error");
+            }
+
+            return new RedirectToPageResult("./UserInformation");
+
+        }
+    }
+}
--- Platibus.Web/Pages/Personal/UserInformation.cshtml.cs
+++ Platibus.Web/Pages/Personal/UserInformation.cshtml.cs
@@ -1,17 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Platibus.Web.Acquaintance.IDataServices;
+using Platibus.Web.DataServices.Models.User;
 
 namespace Platibus.Web.Pages.User_Pages
 {
     public class UserInformationModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly IUserDataService _userDataService;
 
+        [BindProperty] public User user { get; set; }
+        [BindProperty] public string idString { get; set; }
+        [BindProperty] public string accessString { get; set; }
+
+        public UserInformationModel(IUserDataService dataService)
+        {
+            _userDataService = dataService;
         }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            //Change so it is the currently logged in User!!!
+            var id = "387eb00f-7420-45e9-abd9-a5229554115f";
+            try
+            {
+                user = await _userDataService.GetUserById(Guid.Parse(id));
+            }
+            catch (HttpRequestException ex)
+            {
+                return new RedirectToPageResult("Error");
+            }
+
+            idString = user.Id.ToString();
+
+            switch (user.AccesLevel)
+            {
+                case 0:
+                    idString = "Role undefined";
+                    break;
+                case 1:
+                    idString = "Employee";
+                    break;
+                case 2:
+                    idString = "Manager";
+                    break;
+                case 3:
+                    idString = "Administrative Manager";
+                    break;
+                case 4:
+                    idString = "Admin / Root";
+                    break;
+            }
+            
+            return Page();
+        }
+
+        
+
+
     }
 }
