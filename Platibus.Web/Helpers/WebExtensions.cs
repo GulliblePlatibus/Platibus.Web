201ed9de8a5dbae82e4e59b67c73b1bb744f53a3
Ulrik Sandberg <UJSandberg@gmail.com>
2018-11-28 16:18:17 +0100

    Changes to web extensions to issue roles on behalf of new access role enum

--- Platibus.Web/Helpers/WebExtensions.cs
+++ Platibus.Web/Helpers/WebExtensions.cs
@@ -39,25 +39,25 @@ namespace Platibus.Web.Helpers
                 var authLvl = claims.Where(x => x.Type.Equals(TypeName)).SingleOrDefault();
                 
                 //Try and parse the authLvl to a int value;
-                int authNiveau = 0;
-                if (Int32.TryParse(authLvl.Value, out authNiveau))
+
+                if (authLvl.Value.Equals(UserRoles.Admin.ToString()))
                 {
-                    //Issue appropriate role
-                    switch (authNiveau)
-                    {
-                        case 0:
-                            context.Principal.AddIdentity(new ClaimsIdentity(new[]{new Claim(ClaimTypes.Role, UserRoles.Admin.ToString())}));
-                            break;
-                        case 1:
-                            context.Principal.AddIdentity(new ClaimsIdentity(new[]{new Claim(ClaimTypes.Role, UserRoles.Manager.ToString())}));
-                            break;
-                        case 2:
-                            context.Principal.AddIdentity(new ClaimsIdentity(new[]{new Claim(ClaimTypes.Role, UserRoles.Employee.ToString())}));
-                            break;
-                        default:
-                            context.Principal.AddIdentity(new ClaimsIdentity(new[]{new Claim(ClaimTypes.Role,UserRoles.Unknown.ToString())}));
-                            break;
-                    }
+                    context.Principal.AddIdentity(new ClaimsIdentity(new[]{new Claim(ClaimTypes.Role, UserRoles.Admin.ToString())}));
+                }
+
+                if (authLvl.Value.Equals(UserRoles.Administrative.ToString()))
+                {
+                    context.Principal.AddIdentity(new ClaimsIdentity(new[]{new Claim(ClaimTypes.Role, UserRoles.Administrative.ToString())}));
+                }
+
+                if (authLvl.Value.Equals(UserRoles.Manager.ToString()))
+                {
+                    context.Principal.AddIdentity(new ClaimsIdentity(new[]{new Claim(ClaimTypes.Role, UserRoles.Employee.ToString())}));
+                }
+
+                if (authLvl.Value.Equals(UserRoles.Employee.ToString()))
+                {
+                    context.Principal.AddIdentity(new ClaimsIdentity(new[]{new Claim(ClaimTypes.Role,UserRoles.Unknown.ToString())}));
                 }
             }
             
